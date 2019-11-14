07fe7d146d8bce929bdeec3abab0308c1c5d89ce
Gilad Ehven <gilad@ehven.com>
2019-11-14 01:14:13 -0500

    Added 'wp-config' code for 'Bedrock' in new '/merge' directory (for snippets that are MERGED to projects).

---
+++ merge/bedrock/config/application.php
@@ -0,0 +1,21 @@
+<?php
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                               *
+ *              C U S T O M I Z E   B Y   M E R G E              *
+ *                                                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+define( 'AUTOSAVE_INTERVAL', 120 );
+
+define( 'WP_POST_REVISIONS', 4 );
+
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                               *
+ *              E N A B L E   A S   R E Q U I R E D              *
+ *                                                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+ //  if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') $_SERVER['HTTPS'] = 'on';
